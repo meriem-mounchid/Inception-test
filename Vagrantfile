bcf5bf021d29e800169066895adbe80b47496009
Meriem Mounchid <mmounchi@e2r9p13.1337.ma>
2022-02-21 17:42:26 +0100

    modif hostname

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,8 @@
 Vagrant.configure("2") do |config|
   config.vm.box = "ubuntu/trusty64"
   config.vm.box_url = "ubuntu/trusty64"
-	config.vm.define "Misaki" do |controle|
-    controle.vm.hostname = "Misaki"
+	config.vm.define "misaki" do |controle|
+    controle.vm.hostname = "misaki"
     controle.vm.network "public_network", bridge: "en0: Ethernet"
     controle.vm.provider "virtualbox" do |v|
       v.customize ["modifyvm", :id, "--cpus", "2"]
--- script.sh
+++ script.sh
@@ -1,6 +1,25 @@
 #### Install Docker ####
 sudo apt-get update
 sudo apt-get install -y docker.io
-newgrp docker
-#sudo usermod -aG sudo vagrant
+sudo usermod -aG docker vagrant
 
+#### Install Debian ####
+docker pull debian
+docker run debian
+
+# docker images
+# docker ps -a
+# docker rmi -f
+# docker rm -f ID
+
+### TEST ###
+docker pull mysql
+docker run -d mysql:latest
+docker run -d -p 6379:6379 --name redis redis
+docker run -d -p 6379:6379 --name redis -v /tmp/data:/data redis
+# ==> ls /tmp/data/
+docker run -d -p 0.0.0.0:80:80 mysql:latest
+docker run -it --name sl mysql /bin/bash
+docker start sl
+docker ps # listed container
+### END ###
