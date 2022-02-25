5ec0e43c00d78117433286d28e6a690a4c2237bc
Meriem Mounchid <mmounchi@e2r9p13.1337.ma>
2022-02-25 17:24:30 +0100

    adminer with MariaDB done

--- mariadb.sh
+++ mariadb.sh
@@ -2,8 +2,8 @@
 docker run -it -p 0.0.0.0:3306:3306 --name myMariaDB debian
 
 #### Install MariaDB ####
-apt update
-apt install -y mariadb-server
+apt-get update
+apt-get install -y mariadb-server
 
 service mariadb start
 mariadb
@@ -14,12 +14,12 @@ CREATE USER 'master'@localhost IDENTIFIED BY 'master';
 CREATE USER 'master'@'%' IDENTIFIED BY 'master';
 GRANT ALL PRIVILEGES ON wordpress.* TO 'master'@localhost;
 CREATE USER 'worker'@localhost IDENTIFIED BY 'worker'; 
-FLUSH PRIVILEGES;
 
 # Save Changes
 FLUSH PRIVILEGES;
 # See details: SHOW GRANTS FOR 'myuser'@localhost;
 # Delet User: DROP USER myuser@localhost;
 # See All Users: select user from mysql.user;
-# Connect: mysql -u 
+# Connect: mysql -u NAME -p
 
+# ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('root');
